c2d05aaf24abfb827168a8708f806fb347fe4c7a
cookcloud <cookcloud@aliyun.com>
2019-07-28 10:28:54 +0800

    0728

--- src/main/java/com/hy/bean/Employee.java
+++ src/main/java/com/hy/bean/Employee.java
@@ -2,10 +2,11 @@ package com.hy.bean;
 
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
-@Data
+@Component
 public class Employee implements Serializable {
 
     private static  final long serialVersionUID=1l;
@@ -14,4 +15,45 @@ public class Employee implements Serializable {
     private String   email;
     private String gender;
 
+    public Employee() {
+    }
+
+    public Employee(Integer id, String lastName, String email, String gender) {
+        this.id = id;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
