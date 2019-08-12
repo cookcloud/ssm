754f47d3e74f054b58a13643bb56a9854a89b41e
liudongwei <liudongwei@cmhi.chinamobile.com>
2019-08-12 12:17:07 +0800

    OCT stash测试

---
+++ src/main/java/com/hy/smartzonetest/Test2.java
@@ -0,0 +1,28 @@
+/*
+ * Project: ssm
+ *
+ * File Created at 2019/8/12
+ *
+ * Copyright 2016 CMCC Corporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * ZYHY Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license.
+ */
+
+package com.hy.smartzonetest;
+
+/**
+ * @author liudongwei
+ * @version V
+ * @Type Test2
+ * @Desc
+ * @date 2019年08月12日 9:41
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("genggaiue ");
+    }
+}
---
+++ src/main/java/com/hy/smartzonetest/Test5.java
@@ -0,0 +1,28 @@
+/*
+ * Project: ssm
+ *
+ * File Created at 2019/8/12
+ *
+ * Copyright 2016 CMCC Corporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * ZYHY Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license.
+ */
+
+package com.hy.smartzonetest;
+
+/**
+ * @author liudongwei
+ * @version V
+ * @Type Test5
+ * @Desc
+ * @date 2019年08月12日 11:43
+ */
+public class Test5 {
+    public static void main(String[] args) {
+        System.out.println("xinajin55");
+    }
+}
